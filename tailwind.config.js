5f6dfa0064d3e65efb09496a91250018ce7065f3
girish-prajapati <jitubhaiprajapati9173092066@gmail.com>
2025-03-07 12:32:14 +0530

    Tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['tailwind-web.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        serif:[
+          '"Playfair Display"'
+        ]
+      }
+    },
+  },
+  plugins: [],
+}
+
